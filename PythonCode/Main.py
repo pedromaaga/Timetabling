64e22acb9f19187e1128be0a3d7f66c24d3212d6
pedromaaga <pedroaug.magalhaes@gmail.com>
2024-04-05 23:14:03 +0100

    Hard Constraints

--- PythonCode/Main.py
+++ PythonCode/Main.py
@@ -14,7 +14,7 @@ sleep_time = ['20:00','20:00','20:00','20:00','20:00','20:00','20:00']
 
 initial_temperature = 1000
 cooling_rate = 0.99
-max_iterations = 1000
+max_iterations = 20000
 num_runs = 5
 
 # Create time slots
@@ -28,3 +28,11 @@ assignments = CreateObjectAssignments(df_assignments,all_slots)
 
 # Think about how to do the optimization
 TimeTabling = runOptimizationProgram(assignments, initial_temperature, cooling_rate, max_iterations, num_runs)
+
+Z, overview = ObjectiveFunction(TimeTabling)
+print(Z)
+for overview_HC in overview:
+    
+    for string in overview_HC:
+        print(string)
+    print('\n')
--- PythonCode/OptimizationFunctions.py
+++ PythonCode/OptimizationFunctions.py
@@ -5,7 +5,10 @@ from OtherFunctions import *
 ## Main functions
 def runOptimizationProgram(assignments, initial_temperature, cooling_rate, max_iterations, num_runs):
 
-    best_solution = RunAnnealingMultipleTimes(assignments, initial_temperature, cooling_rate, max_iterations, num_runs)
+    if IsThereNeighboors(assignments):
+        best_solution = RunAnnealingMultipleTimes(assignments, initial_temperature, cooling_rate, max_iterations, num_runs)    
+    else:
+        best_solution = GenerateInitialSolution(assignments)
 
     return best_solution
 
@@ -21,14 +24,17 @@ def AnnealingAlgorithm(assignments, initial_temperature, cooling_rate, max_itera
         # 4 - Generate a neighbour from the current solution
         neighbor_solution = GenerateNeighboors(current_solution)
         # 5 - Evaluate
-        current_cost = ObjectiveFunction(current_solution)
-        neighbor_cost = ObjectiveFunction(neighbor_solution)
+        current_cost, _ = ObjectiveFunction(current_solution)
+        neighbor_cost, _ = ObjectiveFunction(neighbor_solution)
         cost_diff = neighbor_cost - current_cost
 
         if cost_diff < 0 or random.random() < math.exp(-cost_diff / current_temperature):
             current_solution = neighbor_solution
 
-        if ObjectiveFunction(current_solution) < ObjectiveFunction(best_solution):
+        current_cost, _ = ObjectiveFunction(current_solution)
+        best_cost, _ = ObjectiveFunction(neighbor_solution)
+
+        if current_cost < best_cost:
             best_solution = current_solution
 
         current_temperature *= cooling_rate
@@ -41,7 +47,7 @@ def RunAnnealingMultipleTimes(assignments, initial_temperature, cooling_rate, ma
 
     for _ in range(num_runs):
         current_solution = AnnealingAlgorithm(assignments, initial_temperature, cooling_rate, max_iterations)
-        current_cost = ObjectiveFunction(assignments)
+        current_cost, _ = ObjectiveFunction(assignments)
 
         if current_cost < best_cost:
             best_solution = current_solution
@@ -72,10 +78,79 @@ def GenerateInitialSolution(assignments):
         assignment.period_scheduled = period
     return assignments
 
+def IsThereNeighboors(assignments):
+
+    for assignment in assignments:
+        qnt_setslots = 0
+        for i in range(0,len(assignment.periods)):
+            qnt_setslots = qnt_setslots + len(assignment.periods[i].available)
+        
+        if qnt_setslots > assignment.qnt_week:
+            return True
+
+    return False
+
 ## Cost function
 def ObjectiveFunction(assignments):
-    return 10
+    # Weights
+    W_HC = [1000, 1000]
+    all_overview = []
+    # Hard Constraints
+    Z_HC_1, overviewHC1 = HardConstraint1(assignments)
+    all_overview.append(overviewHC1)
+    Z_HC_2, overviewHC2 = HardConstraint2(assignments)
+    all_overview.append(overviewHC2)
+
+    Z_HC = W_HC[0]*Z_HC_1 + W_HC[1]*Z_HC_2
+    Z = Z_HC
+
+    return Z, all_overview
 
 ## Constraint functions
 
 # Hard constraints
+# 1 - All assignments needs to be scheduled
+def HardConstraint1(assignments):
+
+    overviewHC1 = ['Overview Hard Constraint 1']
+    sum = 0
+    for assignment in assignments:
+        if assignment.period_scheduled == 0:   # 0 is the default value
+            overviewHC1.append(f'Violation - {assignment.name} is not scheduled')
+            sum = sum + 1
+    if sum == 0:
+        overviewHC1.append('All assignments are scheduled.')
+    
+    return sum, overviewHC1
+
+# 2 - Only one assignment must be scheduled in a specific set of slots
+def HardConstraint2(assignments):
+
+    overviewHC2 = ['Overview Hard Constraint 2']
+    sum = 0
+    for index, current_assignment in enumerate(assignments):
+        slots_current_assignment = []
+        for key, value in current_assignment.period_scheduled.items():
+            slots_current_assignment.append(value['set'])
+        
+        for i in range(index+1,len(assignments)):
+            slots_next_assignment = []
+            next_assignment = assignments[i]
+            for key, value in next_assignment.period_scheduled.items():
+                slots_next_assignment.append(value['set'])
+
+            # Check for overlap between sets
+            flatten_slots_current = [slot for sublist in slots_current_assignment for slot in sublist]
+            flatten_slots_next = [slot for sublist in slots_next_assignment for slot in sublist]
+            
+            if any(slot in flatten_slots_next for slot in flatten_slots_current):
+                overviewHC2.append(f'Violation: Overlapping slots between assignments {current_assignment.name} and {next_assignment.name}')
+                sum += 1
+
+    if sum == 0:
+        overviewHC2.append('No overlapping intervals between assessments.')
+
+    return sum, overviewHC2
+
+
+
